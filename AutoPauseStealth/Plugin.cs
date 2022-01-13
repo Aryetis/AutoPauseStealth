79b06c98dd689001b63ae82da7b291531d49835e
aryetis <aramirg@hotmail.fr>
2022-01-13 16:56:01 +0100

    bumping version number, updating harmony unpatch method

--- AutoPauseStealth/Plugin.cs
+++ AutoPauseStealth/Plugin.cs
@@ -70,7 +70,8 @@ namespace AutoPauseStealth
             try
             {
                 // Removes all patches with this HarmonyId
-                harmony.UnpatchAll(HarmonyId);
+                //harmony.UnpatchAll(HarmonyId);
+                harmony.UnpatchSelf();
             }
             catch (Exception ex)
             {
